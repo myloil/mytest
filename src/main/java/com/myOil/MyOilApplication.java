47cc9b0d51fd64c42cd4c6cc588612c6c17b097e
yshsz <yshsz@cn.ibm.com>
2018-01-08 15:03:05 +0800

    change project to spring boot

---
+++ src/main/java/com/myOil/MyOilApplication.java
@@ -0,0 +1,12 @@
+package com.myOil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOilApplication.class, args);
+	}
+}
